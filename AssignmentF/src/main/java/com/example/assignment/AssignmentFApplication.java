882628a7cd3a96374f595213ac13606099142c02
prachi.patidar <prachi.patidar@ATMECSINLT-226.ATMECS.CORP>
2020-06-08 20:04:09 +0530

    Gradle Updated Task

---
+++ AssignmentF/src/main/java/com/example/assignment/AssignmentFApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentFApplication.class, args);
+	}
+
+}
---
+++ AssignmentF/src/main/java/com/example/assignment/dao/contractRepo.java
@@ -0,0 +1,16 @@
+package com.example.assignment.dao;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.assignment.Model.contractModel;
+
+@Repository
+public interface contractRepo  extends JpaRepository<contractModel,String> {
+
+	boolean existsBycontractName(String name);
+
+	List<contractModel> findBycontractName(String name);
+
+	contractModel findByresourceId(String id);
+}
